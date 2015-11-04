4e295ec493421a7fbda96f74283e4afaedecdf20
Taehupo <uzumakigip@gmail.com>
2015-11-04 11:02:11 +0100

    Ajout code

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
+<!DOCTYPE HTML>
+  <html>
+    <head>
+      <meta charset = "utf-8">
+      <title>LE SITE DU TURFU</title>
+    </head>
 
+    <body>
+
+      Salut ! ceci est le site du
+      <?php
+        echo " TURFU !";
+      ?>
+
+    </body>
